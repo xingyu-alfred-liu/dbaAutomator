b67a197743a75fbc89ecece64c782d6a32b93b1f
Alfred Liu <xingyul1@andrew.cmu.edu>
2019-06-11 21:42:03 +0800

    Now can find the hole positions
    Next should also return the index of the sites
    
    Also need to check if the found hole positions are correct

--- src/locateHole.py
+++ src/locateHole.py
@@ -10,6 +10,10 @@ import numpy as np
 from selectSingleMol import constructSuperCell, getBondDict
 from pymatgen import Molecule
 from bondLengthRef import bondCutoff
+from copy import deepcopy
+import math
+import time
+from itertools import filterfalse
 
 def loadSingleMol(singleMolPath):
     singleMol = Molecule.from_file(singleMolPath+'/singleMol.xyz')
@@ -39,19 +43,66 @@ def getChargeMatrix(singleMol, singleMolPath):
     # returned chargeMatrix provides the charge percentage and atom index
     return chargeMatrix
 
+def calNormalVector(p1, p2, p3):
+    # generate the vector
+    vector = [0, 0, 0]
+    vector[0] = (p2[1]-p1[1])*(p3[2]-p1[2]) - (p2[2]-p1[2])*(p3[1]-p1[1])
+    vector[1] = (p2[2]-p1[2])*(p3[0]-p1[0]) - (p2[0]-p1[0])*(p3[2]-p1[2])
+    vector[2] = (p2[0]-p1[0])*(p3[1]-p1[1]) - (p2[1]-p1[1])*(p3[0]-p1[0])
+    # normalize it
+    sigma = vector[0]**2 + vector[1]**2 + vector[2]**2
+    sigma = math.sqrt(sigma)
+    for i in range(3):
+        vector[i] = vector[i] / sigma
+    return vector
+
+def findHole(supercell, twoNeighbors, chargeSite):
+    point1 = deepcopy(twoNeighbors[0][0].coords)
+    point2 = deepcopy(twoNeighbors[1][0].coords)
+    point3 = deepcopy(chargeSite.coords)
+    normalVec = calNormalVector(point1, point2, point3)
+    shift = -0.8
+    holePosition = [0, 0, 0]
+    for i in range(3):
+        holePosition[i] = chargeSite.coords[i] + normalVec[i]*shift
+    print()
+    print('Hole Position')
+    print(holePosition)
+    print('charge Site')
+    print(chargeSite.coords)
+    supercell.append('He', holePosition, coords_are_cartesian=True)
+    print('Fractional position')
+    print(supercell.sites[-1].frac_coords)
+    return supercell.sites[-1].frac_coords
+
 # !!! important !!!
 # this function sets the charge percentage threshold as 1%
-def getHolePositions(chargeMatrix, singleMol, supercell, bondDict, chargeThreshold=0.1):
+def getHolePositions(chargeMatrix, singleMol, supercell, bondDict, chargeThreshold=0.01):
     bondlength = 0
     for key in bondDict:
         if bondDict[key] >= bondlength:
             bondlength = bondDict[key]
-    print(supercell.sites[0].frac_coords)
     chargeIndex = np.where(chargeMatrix[:, 4] > chargeThreshold)[0]
-    holePositions = []
+    holePositions = dict()
     for charindex in chargeIndex:
         chargeSite = singleMol.sites[charindex]
-        neighborSites = singleMol.get_neighbors(singleMol.sites[charindex], bondlength)
+        neighborSites = singleMol.get_neighbors(chargeSite, bondlength)
+        twoNeighbors = []
+        # delete the neighbors which are H
+        neighborSites[:] = filterfalse(lambda x: str(x[0].specie) == 'H', neighborSites)
+        for neighbor in neighborSites:
+            if len(twoNeighbors) < 2:
+                twoNeighbors += [neighbor]
+            else:
+                twoNeighbors += [neighbor]
+                tmpBondLength = 0
+                for site in twoNeighbors:
+                    if site[1] > tmpBondLength:
+                        tmpBondLength = site[1]
+                        removeSite = site
+                twoNeighbors.remove(removeSite)
+        holePositions[chargeSite] = findHole(supercell, twoNeighbors, chargeSite)
+
     return None
 
 if __name__ == "__main__":
